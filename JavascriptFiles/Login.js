9b07f055b9b29bb6f8737fb9b1d17acda14f9afd
Maria Rajabzadeh <maria.rajabzadehnamaghi.5780@student.uu.se>
2016-02-06 20:08:59 +0100

    New folders and files

--- JavascriptFiles/Login.js
+++ JavascriptFiles/Login.js
@@ -1,25 +1,46 @@
-/**
- * Created by MA on 2/2/2016.
- */
-function OnSubmitForm()
-{
+function f1(){
+
+    //validation
+
+    //event.preventDefault();
     var userElem = document.getElementById("username");
+    //  console.log(userElem);
     var passElem = document.getElementById("password");
     var defUser = userElem.defaultValue;
     var defPass = passElem.defaultValue;
 
     if(userElem.value == "" || passElem.value == "" || userElem.value == defUser || passElem.value == defPass){
         alert("username and password must be filled out");
-        return false;
+        return;
     }
 
-    link =  "http://pub.jamaica-inn.net/fpdb/api.php?";
-    link += "username=" + userElem.value + "&password=" + passElem.value;
-    link += "&action=iou_get";
-
-   // document.LoginForm.action = link;
-
-  //  return document.LoginForm.runvalidation();
 
 
+
+
+    var xmlhttp = new XMLHttpRequest();
+
+
+    var linku =  "http://pub.jamaica-inn.net/fpdb/api.php?";
+    linku += "username=" + userElem.value + "&password=" + passElem.value;
+    linku += "&action=iou_get";
+    var url = linku;
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            myFunction(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+function myFunction(response) {
+    var arr = JSON.parse(response);
+    var i;
+    var out = "";
+alert(arr.payload[0].first_name);
+
+        out += arr.type;
+
+    document.getElementById("id01").innerHTML = out;
 }
