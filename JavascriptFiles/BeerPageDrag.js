9a0f756644489b3edba223bde40c12da002708db
Maria Rajabzadeh <maria.rajabzadehnamaghi.5780@student.uu.se>
2016-02-11 12:37:59 +0100

    drag

--- JavascriptFiles/BeerPageDrag.js
+++ JavascriptFiles/BeerPageDrag.js
@@ -1,3 +1,17 @@
 /**
  * Created by MA on 2/10/2016.
  */
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
