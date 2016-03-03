d3625c5646f130695a738435e15cc8cab091540c
Jessica <jessica121353@gmail.com>
2016-03-03 01:18:56 +0100

    search by name

--- JavascriptFiles/beermenu.js
+++ JavascriptFiles/beermenu.js
@@ -1,32 +1,3 @@
-$(function getAllMenu() {
-
-    $("#beerTable").show();
-
-    $.getJSON("Database/Database_Beer.json", function(json) {
-
-        var count = json.length;
-        for(var i = 0; i<count; i++) {
-            var tmp = "";
-            var beer_id = json[i].beer_id;
-            var name = json[i].namn;
-            var name2 = json[i].namn2;
-            var price = json[i].pub_price;
-            if(i%2==0) {
-                tmp += '<tr class="menuRowEven" id="' + beer_id + '" onmouseover="mouseOverForDrag()" draggable="true" ondragstart="drag(event)">' +
-                    '<td class="beerName"><img src="CSSFiles/images/beer.png" width="10%"><span title="' + name2 + '">' + name + '</span>' + '</td>'
-                    + '<td class="beerPrice">' + price + " SEK" + '</td></tr>';
-            }
-            else {
-                tmp += '<tr class="menuRow" id="' + beer_id + '" onmouseover="mouseOverForDrag()" draggable="true" ondragstart="drag(event)">' +
-                    '<td class="beerName"><img src="CSSFiles/images/beer.png" width="10%"><span title="' + name2 + '">' + name + '</span>' + '</td>'
-                    + '<td class="beerPrice">' + price + " SEK" + '</td></tr>';
-            }
-            $(tmp).appendTo("#beerTable");
-        }
-
-    });
-
-});
 function buildMenu(tmp, beer_id, name, name2, price, allergy, counter){
     if(counter%2==0) {
         tmp += '<tr class="menuRowEven" id="' + beer_id + '" onmouseover="mouseOverForDrag()" draggable="true" ondragstart="drag(event)">' +
@@ -41,6 +12,28 @@ function buildMenu(tmp, beer_id, name, name2, price, allergy, counter){
     console.log(name+" "+allergy);
     $(tmp).appendTo("#beerTable");
 }
+
+$(function getAllMenu() {
+
+    $("#beerTable").show();
+
+    $.getJSON("Database/Database_Beer.json", function(json) {
+
+        var count = json.length;
+        for(var i = 0; i<count; i++) {
+            var tmp = "";
+            var beer_id = json[i].beer_id;
+            var name = json[i].namn;
+            var name2 = json[i].namn2;
+            var price = json[i].pub_price;
+            var allergy = json[i].allergy;
+            buildMenu(tmp, beer_id, name, name2, price, allergy, i);
+        }
+
+    });
+
+});
+
 function filter() {
     var Gchecked = document.getElementById('gulten').checked;
     var Lchecked = document.getElementById('lactose').checked;
@@ -58,31 +51,31 @@ function filter() {
             var price = json[i].pub_price;
             var allergy = json[i].allergy;
             var tmp = "";
-            if(Gchecked && Lchecked && Nchecked && allergy=="") {
+            if(Gchecked && Lchecked && Nchecked && allergy==="") {
                buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
-            else if(Gchecked && Lchecked && !Nchecked && allergy=="nut") {
+            else if(Gchecked && Lchecked && !Nchecked && allergy==="nut") {
                 buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
-            else if(Gchecked && !Lchecked && Nchecked && allergy=="lactose") {
+            else if(Gchecked && !Lchecked && Nchecked && allergy==="lactose") {
                 buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
-            else if(!Gchecked && Lchecked && Nchecked && allergy=="gluten"){
+            else if(!Gchecked && Lchecked && Nchecked && allergy==="gluten"){
                 buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
-            else if(Gchecked && !Lchecked && !Nchecked && allergy!="gluten") {
+            else if(Gchecked && !Lchecked && !Nchecked && allergy!=="gluten") {
                 buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
-            else if(!Gchecked && Lchecked && !Nchecked && allergy!="lactose") {
+            else if(!Gchecked && Lchecked && !Nchecked && allergy!=="lactose") {
                 buildMenu(tmp, beer_id, name, name2, price, allergy, counter);
                 counter++;
             }
-            else if(!Gchecked && !Lchecked && Nchecked && allergy!='nut') {
+            else if(!Gchecked && !Lchecked && Nchecked && allergy!=='nut') {
                 buildMenu(tmp, beer_id, name, name2, price, allergy,counter);
                 counter++;
             }
@@ -90,4 +83,25 @@ function filter() {
         }
 
     });
+}
+
+function search(searchString) {
+    searchString = searchString.toLowerCase();
+    document.getElementById('beerTable').innerHTML = "";
+
+    $.getJSON("Database/Database_Beer.json", function(json) {
+
+        var results = $.map(json, function(entry) {
+            var match = (entry.namn.toLowerCase().indexOf(searchString)!== -1) || (entry.namn2.toLowerCase().indexOf(searchString)!==-1);
+            return match ? entry : null;
+        });
+        var count = results.length;
+        for(var i = 0; i<count; i++) {
+            var tmp = "";
+            buildMenu(tmp, results[i].beer_id, results[i].namn, results[i].namn2, results[i].price, results[i].allergy, i);
+            $(tmp).appendTo("#beerTable");
+        }
+
+    });
+
 }
--- JavascriptFiles/beerpage_language.js
+++ JavascriptFiles/beerpage_language.js
@@ -25,6 +25,8 @@ function change_lang()
     document.getElementById('nutAllergi').innerHTML = myLangArray[2];
     $('#filterButton').val(myLangArray[3]);
     document.getElementById('blinkDragText').innerHTML = myLangArray[4];
+    document.getElementById('SearchbyName').innerHTML = myLangArray[5];
+    document.getElementById('SearchbyPrice').innerHTML = myLangArray[6];
 
 }
 
@@ -36,6 +38,8 @@ Lang_en[1] = "lactose intolerance";
 Lang_en[2] = "nut allergi";
 Lang_en[3] = "filter";
 Lang_en[4] = "Drag your items below";
+Lang_en[5] = "Search by name";
+Lang_en[6] = "Search by price";
 
 //Swedish
 Lang_se = new Array();
@@ -44,3 +48,5 @@ Lang_se[1] = "laktosintolerant";
 Lang_se[2] = "mutter allergi";
 Lang_se[3] = "filtrera";
 Lang_se[4] = "Dra din saker nedanför";
+Lang_se[5] = "Sök efter namn";
+Lang_se[6] = "Sök efter pris";
